311a919463f2f6cb040e6fbe55115884ff8976cc
Geoffroy Baghdassarian <geoffroy.baghdassarian@gmail.com>
2018-10-30 11:04:04 +0100

    2nd commit

--- js/script.js
+++ js/script.js
@@ -18,11 +18,13 @@ function masterMind(){
 
 // arrayGuess
 var arrayGuess = [0,0,0,0];
+var whiteGuess = 0
+var blackGuess = 0
 
 // test if arrayGuess is correct
 function testGuess(){
-    var whiteGuess = 0
-    var blackGuess = 0
+  whiteGuess = 0
+  blackGuess = 0
 
     //test whites
     arrayGuess.forEach(function(oneColor){
@@ -51,34 +53,15 @@ $(".hole").click(function () {
     holder = (button.attr('name'));
     button.removeClass("yellow white violet green")
     button.addClass("selected");
-    button.next().removeClass("selected");
-    button.next().next().removeClass("selected");
-    button.next().next().next().removeClass("selected");
-    button.prev().removeClass("selected");
-    button.prev().prev().removeClass("selected");
-    button.prev().prev().prev().removeClass("selected");
+    button.siblings().removeClass("selected");
+    if(arrayGuess[holder-1]!==0){
+      console.log("hi")
+    }
 });
+
 
 // push the color picked to the chosen hole
 $(".color-section").click(function () {
-  // if(count==2){
-  //       var buttonColor = $(event.target);
-  //   arrayGuess[holder-1]=buttonColor.attr('name')
-  //   console.log(arrayGuess);  
-  //   var pushColor1 = document.getElementsByName("1");
-  //   $(pushColor1).removeClass("yellow white violet green");
-  //   $(pushColor1).addClass(arrayGuess[0]);
-  //   var pushColor2 = document.getElementsByName("2")
-  //   $(pushColor2).removeClass("yellow white violet green");
-  //   $(pushColor2).addClass(arrayGuess[1]);
-  //   var pushColor3 = document.getElementsByName("3")
-  //   $(pushColor3).removeClass("yellow white violet green");
-  //   $(pushColor3).addClass(arrayGuess[2]);
-  //   var pushColor4 = document.getElementsByName("4")
-  //   $(pushColor4).removeClass("yellow white violet green");
-  //   $(pushColor4).addClass(arrayGuess[3]);
-  // }
-  // if(count>2){
     var buttonColor = $(event.target);
     arrayGuess[holder-1]=buttonColor.attr('name')
     console.log(arrayGuess);  
@@ -94,63 +77,73 @@ $(".color-section").click(function () {
     var pushColor4 = document.getElementsByName("4")
     $(pushColor4[pushColor4.length-1]).removeClass("yellow white violet green");
     $(pushColor4[pushColor4.length-1]).addClass(arrayGuess[3]);
-  // }
+
+    
 
 });
 
-
-var count = 2
-
-
-
-
-
+var count = 1
 
 // check button
 $(".btn-check").click(function () {
     // run function to test if arrays are equal
     testGuess();
-
+    var resBW = $(
+      '<div>B= '+blackGuess+'<br> W= '+ whiteGuess+'</div>')
+    var tempRes = document.getElementsByName("tempresult"+count)
+    $(tempRes).html(resBW)
     // block clics on previous divs after test
     $(".guess").addClass("blocked")
     $(".hole").removeClass("selected")
+   
+    count +=1
     
-    
+    if(blackGuess<4){
     // create new div for new guess
     var newGuess = $(
-    '<div class="guess" name=guess'+count+'">'+ count +
-    '  <div class="hole" name="1"></div>' +
-    '  <div class="hole" name="2"></div>' +
-    '  <div class="hole" name="3"></div>' + 
-    '  <div class="hole" name="4"></div>' +
-    '</div>');
-  
-    // insert new div 
-    $(".btn-check").before(newGuess);
-    count +=1 
+      '<div class="guess" name=guess'+count+'">'+ count +
+      '  <div class="hole" name="1"></div>' +
+      '  <div class="hole" name="2"></div>' +
+      '  <div class="hole" name="3"></div>' + 
+      '  <div class="hole" name="4"></div>' +
+      '  <div class="temp-result" name = "tempresult'+count+'"></div>' +
+      '</div>');
 
-    //reset arrayGuess
-    arrayGuess = [0,0,0,0]
+      // insert new div 
+      $(".btn-check").before(newGuess);
+       
 
-    //function click for new Guess
-    newGuess.find(".hole").click(function(){
-          holder = 0;
-        var button = $(event.target);
-        holder = (button.attr('name'));
-        button.removeClass("yellow white violet green")
-        button.addClass("selected");
-        button.next().removeClass("selected");
-        button.next().next().removeClass("selected");
-        button.next().next().next().removeClass("selected");
-        button.prev().removeClass("selected");
-        button.prev().prev().removeClass("selected");
-        button.prev().prev().prev().removeClass("selected"); 
-  })
+      //reset arrayGuess
+      arrayGuess = [0,0,0,0]
 
+      //function click for new Guess
+      newGuess.find(".hole").click(function(){
+            holder = 0;
+          var button = $(event.target);
+          holder = (button.attr('name'));
+          button.removeClass("yellow white violet green")
+          button.addClass("selected");
+          button.next().removeClass("selected");
+          button.next().next().removeClass("selected");
+          button.next().next().next().removeClass("selected");
+          button.prev().removeClass("selected");
+          button.prev().prev().removeClass("selected");
+          button.prev().prev().prev().removeClass("selected"); 
+    })
+        }
+    else {
+      $(".result").css({"opacity":"1"});;
+      $(".result").css({"display":"flex"});;
+    }
+    
 })
 
 
 // get mastermind at loading of page
 $(document).ready(function(){
     masterMind();
+    $(".result-1").addClass(arrayResult[0]);
+    $(".result-2").addClass(arrayResult[1]);
+    $(".result-3").addClass(arrayResult[2]);
+    $(".result-4").addClass(arrayResult[3]);
     });
